677f731d5d793828341e25d294cde011f0f1a112
melissa bafdel <melissabafdel@gmail.com>
2020-05-12 03:52:48 +0200

    ajout de classes

--- src/main/java/forme/Triangle.java
+++ src/main/java/forme/Triangle.java
@@ -1,12 +1,30 @@
 package forme;
-
+/**
+ * 
+ * @author Melissa
+ *
+ */
 public class Triangle extends Forme {
+	/**
+	 * les trois points du tiangle
+	 */
 	private Position [] points;
-
-	protected Triangle(String newVariableName) {
+/**
+ * constructeur de la classe triangle
+ * @param newVariableName nom de l avariable pour creer le triangle
+ * @param point1 point 1
+ * @param point2 point 2
+ * @param point3 point 3
+ */
+	protected Triangle(String newVariableName, final Position point1, final Position point2, final Position point3) {
 		super(newVariableName);
-		// TODO Auto-generated constructor stub
-	}
+      final int trois = 3;
+	        points = new Position[trois];
+	        points[0] = point1.clone();
+	        points[1] = point2.clone();
+	        points[2] = point3.clone();
+	    }
+	
 
 	@Override
 	public void deplacer(int x, int y) {
