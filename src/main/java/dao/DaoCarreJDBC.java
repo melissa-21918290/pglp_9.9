5adf8da68116a432fbf5284c4ae6e94213af05f8
melissa bafdel <melissabafdel@gmail.com>
2020-05-13 02:21:56 +0200

    ajout des classes DAO

--- src/main/java/dao/DaoCarreJDBC.java
+++ src/main/java/dao/DaoCarreJDBC.java
@@ -1,9 +1,13 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import forme.Carre;
+import forme.Position;
 /**
  * 
  * @author Melissa
@@ -26,35 +30,140 @@ public DaoCarreJDBC(final Connection c){
 
 
 	@Override
-	public Carre create(Carre object) {
+	public Carre create(final Carre object) {
 		// TODO Auto-generated method stub
-		return null;
+	      
+		  final int un = 1, deux = 2, trois = 3, quatre = 4;
+		        try {
+		            PreparedStatement prepare = connect.prepareStatement(
+		                    "INSERT INTO Forme"
+		                    + " (variable)"
+		                    + " VALUES(?)");
+		                    prepare.setString(un, object.getvariable());
+		                    prepare.executeUpdate();
+		            prepare = connect.prepareStatement(
+		                    "INSERT INTO Carre"
+		                    + " (variable,HautGaucheX,HautGaucheY,longeur)"
+		                    + " VALUES(?, ?, ?, ?)");
+		            prepare.setString(un, object.getvariable());
+		            prepare.setInt(deux, object.getHautGauche().getX());
+		            prepare.setInt(trois, object.getHautGauche().getY());
+		            prepare.setInt(quatre, object.getLongeur());
+		            prepare.executeUpdate();
+		        } catch (SQLException e) {
+		            return null;
+		        }
+		        return object;
+		
 	}
-
+/**
+ * recuperer un element
+ * @param id identifiant de l'élément a recuperer
+ * @return l'element
+ */
 	@Override
 	public Carre find(String id) {
 		// TODO Auto-generated method stub
-		return null;
+		 final int un = 1;
+	        Carre find = null;
+	        try {
+	            PreparedStatement prepare = connect.prepareStatement(
+	                    "SELECT * FROM Carre WHERE variable = ?");
+	            prepare.setString(un, id);
+	            ResultSet result = prepare.executeQuery();
+	            if (result.next()) {
+	                Position p = new Position(result.getInt("HautGaucheX"),
+	                        result.getInt("HautGaucheY"));
+	                try {
+	                    find = new Carre(id, p, result.getInt("longeur"));
+	                } catch (Exception e) {
+	                    e.printStackTrace();
+	                    return null;
+	                }
+	            }
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	            return null;
+	        }
+	        return find;
 	}
-
+/**
+ * recuperer tous les elements
+ * @return tous les elements
+ */
 	@Override
 	public ArrayList<Carre> findAll() {
 		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Carre> find = new ArrayList<Carre>();
+        try {
+            PreparedStatement prepare = connect.prepareStatement(
+                    "SELECT variable FROM Carre");
+            ResultSet result = prepare.executeQuery();
+            while (result.next()) {
+                find.add(this.find(result.getString("variable")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return new ArrayList<Carre>();
+        }
+        return find;
 	}
-
+/**
+ * modifier un élément du DAO
+ * @param object l'élément a modifier
+ * @return la modification
+ */
 	@Override
 	public Carre update(Carre object) {
-		// TODO Auto-generated method stub
-		return null;
+		final int un = 1, deux = 2, trois = 3, quatre = 4;
+        final Carre before = this.find(object.getvariable());
+        if (before != null) {
+            try {
+                PreparedStatement prepare = connect.prepareStatement(
+                "UPDATE Carre SET HautGaucheX = ?, HautGaucheY = ?, "
+                + "longeur = ? WHERE variable = ?");
+                prepare.setInt(un, object.getHautGauche().getX());
+                prepare.setInt(deux, object.getHautGauche().getY());
+                prepare.setInt(trois, object.getLongeur());
+                prepare.setString(quatre, object.getvariable());
+                prepare.executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return before;
+            }
+        } else {
+            return null;
+        }
+        return object;
 	}
 
 	@Override
 	public void delete(Carre object) {
 		// TODO Auto-generated method stub
+		   final int un = 1;
+	        try {
+	            this.deleteComposant(object.getvariable());
+	            PreparedStatement prepare = connect.prepareStatement(
+	                    "DELETE FROM Carre WHERE variable = ?");
+	            prepare.setString(un, object.getvariable());
+	            prepare.executeUpdate();
+	            prepare = connect.prepareStatement(
+	                    "DELETE FROM Forme WHERE variable = ?");
+	            prepare.setString(un, object.getvariable());
+	            prepare.executeUpdate();
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	    }
+
+
+	private void deleteComposant(String getvariable) {
+		// TODO Auto-generated method stub
+		
+	}
 		
 	}
 
 	
 
-}
+
