e0c22735e7da102a86b5d7087b5d05e17fbc0aed
melissa bafdel <melissabafdel@gmail.com>
2020-05-15 18:33:40 +0200

    ajout classe Command

--- src/main/java/DeplacerC.java
+++ src/main/java/DeplacerC.java
@@ -1,4 +1,44 @@
-
-public class DeplacerC {
-
+/**
+ * 
+ * @author Melissa
+ *
+ */
+public class DeplacerC implements Command {
+	/**
+	 * forme à déplacer
+	 */
+	private Forme forme;
+	/**
+	 * vecteur de deplacement
+	 */
+	private Position Tab;
+	
+public DeplacerC(final Forme f, final Position TabD){
+	this.Tab=TabD;
+	forme=f;
 }
+	public void executer() {
+		 forme.deplacer(Tab.getX(), Tab.getY());
+	        DaoFactoryJDBC factory = new DaoFactoryJDBC();
+	        if (forme.getClass() == Cercle.class) {
+	            AbstractDao<Cercle> dao = factory.getDaoCercle();
+	            dao.update((Cercle) forme);
+	        } else if (forme.getClass() == Carre.class) {
+	            AbstractDao<Carre> dao = factory.getDaoCarre();
+	            dao.update((Carre) forme);
+	        } else if (forme.getClass() == Rectangle.class) {
+	            AbstractDao<Rectangle> dao = factory.getDaoRectangle();
+	            dao.update((Rectangle) forme);
+	        } else if (forme.getClass() == Triangle.class) {
+	            AbstractDao<Triangle> dao = factory.getDaoTriangle();
+	            dao.update((Triangle) forme);
+	        } else {
+	            AbstractDao<GroupeForme> dao = factory.getDaoGroupeForme();
+	            dao.update((GroupeForme) forme);
+	        }
+	        factory.close();
+	    }
+		
+	}
+
+
