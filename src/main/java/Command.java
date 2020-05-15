2749d778be8d5ab356e3ef9d2ba929dd095022f0
melissa bafdel <melissabafdel@gmail.com>
2020-05-15 18:28:55 +0200

    ajout classe Command

---
+++ src/main/java/Command.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @author Melissa
+ *
+ */
+public interface Command {
+	/**
+	 * executer la commande
+	 */
+	void executer();
+
+}
---
+++ src/main/java/CreationC.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ * @author Melissa
+ *
+ */
+public class CreationC implements Command {
+/**
+ * forme à creer
+ */
+	private Forme forme;
+	/**
+	 * constructeur de la classe
+	 * @param f forme a créer
+	 */
+	public CreationC(final Forme f){
+		forme=f;
+	}
+	
+	public void executer() {
+		Forme f;
+        DaoFactoryJDBC factory = new DaoFactoryJDBC();
+        if (forme.getClass() == Cercle.class) {
+            AbstractDao<Cercle> dao = factory.getDaoCercle();
+            f = dao.create((Cercle) forme);
+        } else if (forme.getClass() == Carre.class) {
+            AbstractDao<Carre> dao = factory.getDaoCarre();
+            f = dao.create((Carre) forme);
+        } else if (forme.getClass() == Rectangle.class) {
+            AbstractDao<Rectangle> dao = factory.getDaoRectangle();
+            f = dao.create((Rectangle) forme);
+        } else if (forme.getClass() == Triangle.class) {
+            AbstractDao<Triangle> dao = factory.getDaoTriangle();
+            f = dao.create((Triangle) forme);
+        } else {
+            AbstractDao<GroupeForme> dao = factory.getDaoGroupeForme();
+            f = dao.create((GroupeForme) forme);
+        }
+        factory.close();
+        if (f != null) {
+            System.out.println("Forme ajoutée avec succes "
+                    + forme.getvariable() );
+        } else {
+            System.out.println("Une forme existe déjà  : "
+                    + forme.getvariable()+"veillez changer de nom ");
+        }
+    }
+
+	
+}
